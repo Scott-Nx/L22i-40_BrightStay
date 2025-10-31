15153bdb269c8c59446bb89777afa61dcbe46f63
Scott-Nx <158021536+Scott-Nx@users.noreply.github.com>
2025-10-31 13:44:42 +0700

    chore: add BrightStay service and script for Linux
    Run on suspend to adjust L22i-40 monitor brightness via ddcutil
    
    Targets product code 26542 and sets VCP 0x10 to 5%

---
+++ linux/scripts/BrightStay.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PRODUCT_CODE="26542"
+
+
+BRIGHTNESS_LEVEL=5
+
+change_brightness() {
+    echo "Changing brightness on $PRODUCT_CODE to $BRIGHTNESS_LEVEL%"
+    # Assuming the monitor's VCP code for brightness is 0x10 (common for many monitors)
+    ddcutil setvcp 0x10 $BRIGHTNESS_LEVEL
+}
+
+if ddcutil detect | grep -q $PRODUCT_CODE; then
+    # If connected, change the brightness
+    change_brightness
+else
+    echo "External monitor with Product Code $PRODUCT_CODE not connected."
+fi
